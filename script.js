6712a47cc8c88f09236bd177dbe08d7e8705fdfc
Tollef Jørgensen <tollefj@gmail.com>
2018-11-02 16:08:08 +0100

    loop music

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-  setTimeout(() => {
-    document.getElementByClassName('hackerman').play();
-  }, 200)
+  myAudio = new Audio('hackerman.mp3'); 
+  var currTime = 0;
+  myAudio.addEventListener('ended', function() {
+      currTime = 0;
+      myAudio.play();
+  }, false);
+  myAudio.play();
   $.getJSON("https://api.ipify.org?format=jsonp&callback=?",
     function(json) {
       $.getJSON('https://json.geoiplookup.io/'+json.ip, function(data) {
